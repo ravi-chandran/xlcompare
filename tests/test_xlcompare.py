0b609a69f94270b756e3f398235f99af0d57e741
Ravi Chandran <rchandran@adacautomotive.com>
2021-05-01 11:34:51 -0400

    Add tests and data

---
+++ tests/test_xlcompare.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import filecmp
+import os
+import pytest
+import subprocess
+
+
+TESTDIR = os.path.dirname(os.path.realpath(__file__))
+
+OLD_XLS = os.path.join(TESTDIR, 'inputs', 'old.xls')
+NEW_XLS = os.path.join(TESTDIR, 'inputs', 'new.xls')
+
+OLD_XLSX = os.path.join(TESTDIR, 'inputs', 'old.xlsx')
+NEW_XLSX = os.path.join(TESTDIR, 'inputs', 'new.xlsx')
+
+EXPECTED = os.path.join(TESTDIR, 'expected', 'diffxls.xlsx')
+
+OUTDIFF = os.path.join(TESTDIR, 'diff.xlsx')
+SAVEDIFF = os.path.join(TESTDIR, 'save.xlsx')
+
+
+def rmfile(filepath):
+    """Remove file if it exists."""
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+
+
+def compare_files(file1, file2):
+    """Compare two binary files."""
+    with open(file1, 'rb') as f:
+        data1 = f.read()
+    with open(file2, 'rb') as f:
+        data2 = f.read()
+    return data1 == data2
+
+
+def compare(cmd):
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    assert result.stderr == ''
+    assert 'Generated' in result.stdout
+    assert 'Done.' in result.stdout
+    assert os.path.isfile(OUTDIFF)
+    # assert compare_files(OUTDIFF, EXPECTED)
+    # filecmp.clear_cache()
+    # assert filecmp.cmp(OUTDIFF, EXPECTED)
+
+
+# Test basic entry point to xlcompare
+def test_entrypoint():
+    exit_status = os.system('xlcompare --help')
+    assert exit_status == 0
+
+
+# Test comparison of .xls vs .xls
+def test_xls_vs_xls():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_XLS, NEW_XLS, '-o', OUTDIFF]
+    compare(cmd)
+    rmfile(OUTDIFF)
+
+
+# Test comparison of .xlsx vs .xlsx
+def test_xlsx_vs_xlsx():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_XLSX, NEW_XLSX, '-o', OUTDIFF]
+    compare(cmd)
+    os.rename(OUTDIFF, SAVEDIFF)
+    rmfile(OUTDIFF)
+
+
+# Test comparison of .xls vs .xlsx
+def test_xls_vs_xlsx():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_XLS, NEW_XLSX, '-o', OUTDIFF]
+    compare(cmd)
+    rmfile(OUTDIFF)
+
+
+# Test comparison of .xlsx vs .xls
+def test_xlsx_vs_xls():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_XLSX, NEW_XLS, '-o', OUTDIFF]
+    compare(cmd)
+    rmfile(OUTDIFF)
