3caf484104e400909aa4dc988bee80ab25341d09
Ravi Chandran <rchandran@adacautomotive.com>
2021-05-02 09:33:46 -0400

    tests: add ID tests

--- tests/test_xlcompare.py
+++ tests/test_xlcompare.py
@@ -15,6 +15,10 @@ NEW_XLSX = os.path.join(TESTDIR, 'inputs', 'new.xlsx')
 OLD_COLS_CHG = os.path.join(TESTDIR, 'inputs', 'old_columns_change.xlsx')
 NEW_COLS_CHG = os.path.join(TESTDIR, 'inputs', 'new_columns_change.xlsx')
 
+OLD_IDTEST = os.path.join(TESTDIR, 'inputs', 'old_idname.xlsx')
+NEW_IDTEST = os.path.join(TESTDIR, 'inputs', 'new_idname.xlsx')
+
+
 # EXPECTED = os.path.join(TESTDIR, 'expected', 'diffxls.xlsx')
 
 OUTDIFF = os.path.join(TESTDIR, 'diff.xlsx')
@@ -103,7 +107,39 @@ def test_col_change():
     rmfile(OUTDIFF)
 
 
+# Tests with bad IDs
+def test_bad_id_xls():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_XLS, NEW_XLS, '-o', OUTDIFF, '--id', 'BAD_ID']
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    assert 'ERROR: Column BAD_ID not found in' in result.stdout
+    assert result.returncode == 1
+    assert not os.path.isfile(OUTDIFF)
 
+
+def test_bad_id_xlsx():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_XLSX, NEW_XLSX, '-o', OUTDIFF, '--id', 'BAD_ID']
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    assert 'ERROR: Column BAD_ID not found in' in result.stdout
+    assert result.returncode == 1
+    assert not os.path.isfile(OUTDIFF)
+
+
+def test_non_existent_id():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_IDTEST, NEW_IDTEST, '-o', OUTDIFF]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    assert 'ERROR: Column ID not found in' in result.stdout
+    assert result.returncode == 1
+    assert not os.path.isfile(OUTDIFF)
+
+
+# Test with non-standard good ID
+def test_non_standard_good_id():
+    rmfile(OUTDIFF)
+    cmd = ['xlcompare', OLD_IDTEST, NEW_IDTEST, '-o', OUTDIFF, '--id', 'REQID']
+    result = verify_common(cmd)
 
 # TODO:
 # Test for --id
